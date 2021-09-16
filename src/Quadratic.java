7ffba6733d1aa518783c8f4c08b02cfa59c8a1e8
Tyler Carter <70717266+TC5555@users.noreply.github.com>
2021-09-16 10:37:11 -0700

    not finished but moving on temporarily.

--- src/Quadratic.java
+++ src/Quadratic.java
@@ -1,4 +1,7 @@
 public class Quadratic {
+
+    import java.util.
+
     public static double posequation(double a, double b, double c) {
         double answer;
         answer = ((-1 * b) + Math.sqrt((b * b) - (4 * a * c))) / (2 * a);
@@ -12,10 +15,10 @@ public class Quadratic {
     }
 
     public static void main(String[] args) {
-        System.out.println("X = " + posequation(1,-7,12) + ", X = " + negequation(1,-7,12));
+        double a, b, c;
 
-        System.out.println("X = " + posequation(1,3,2) + ", X = " + negequation(1,3,2));
+        System.out.println(a + " " + b + " " + c);
 
-        System.out.println("X = " + posequation(1,3,2) + ", X = " + negequation(1,3,2));
+        System.out.println("X = " + posequation(a,b,c) + ", X = " + negequation(a,b,c));
     }
 }
