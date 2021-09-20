f5aa25ea724123c5899b6ae08f61b39209462c09
Tyler Carter <70717266+TC5555@users.noreply.github.com>
2021-09-20 10:10:21 -0700

    triangle completed

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,17 +1,35 @@
+import java.util.*;
+
 public class Triangle {
 
-    public static double angleA(double a, double b, double c) {
+        public static double angleA(double a, double b, double c) {
         double cosine, angle;
-        cosine = ((c * c) - ((a * a) + (b * b))) / (-2 * a *b);
-        angle = Math.acos(cosine);
+        cosine = ((c * c) - ((a * a) + (b * b))) / (-2 * a * b);
+        angle = Math.toDegrees(Math.acos(cosine));
+        return angle;
+    }
+
+    public static double angleB(double a, double b, double c) {
+        double cosine, angle;
+        cosine = ((b * b) - ((a * a) + (c * c))) / (-2 * a * c);
+        angle = Math.toDegrees(Math.acos(cosine));
         return angle;
     }
 
     public static void main(String[] args) {
-        double a = 10, b = 4, c = 7;
-        double answer = angleA(a, b, c);
-        System.out.print(answer + " success");
+
+        Scanner console = new Scanner(System.in);
+
+        System.out.print("a = ");
+        double a = console.nextDouble();
+
+        System.out.print("b = ");
+        double b = console.nextDouble();
+
+        System.out.print("c = ");
+        double c = console.nextDouble();
+
+        double answer1 = angleA(a, b, c), answer2 = angleB(a, b, c);
+        System.out.println("angle a = " + answer1 + " angle b = " + answer2 + " angle c = " + (180 - (answer1 + answer2)));
     }
-
-
 }
