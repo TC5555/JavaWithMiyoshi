b9e5e5a4330e3d25402790115fe5b52e6391afa3
Tyler Carter <70717266+TC5555@users.noreply.github.com>
2021-09-22 09:55:06 -0700

    finished repl

--- src/repl.java
+++ src/repl.java
@@ -1,5 +1,15 @@
 public class repl {
-    public static String rep(String ){
+    public static String rep(String tocount, int countnum){
+        String countproduct = "";
+        for(int i = 0; i < countnum; i++) {
+            countproduct = countproduct + tocount;
+        }
+        return countproduct;
+    }
+    public static void main(String[] args) {
+        int countnum = 5;
+        String tocount = "Hello";
+        System.out.println(rep(tocount, 5));
 
     }
 }
