b413679dfaa5403798f06f4ca1903cc67b823b1d
Tyler Carter <70717266+TC5555@users.noreply.github.com>
2021-09-30 10:44:36 -0700

    finished reverse string

--- src/printReverse.java
+++ src/printReverse.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class printReverse {
         public static void main(String[] args) {
             Scanner console = new Scanner(System.in);
-            console.delimiter()
             System.out.print("enter string: ");
-            String ToReverse = console.next(), Reversed = "";
+            String ToReverse = console.nextLine(), Reversed = "";
 
             for (int i = ToReverse.length() - 1; i >= 0; i--) {
                 char c = ToReverse.charAt(i);
