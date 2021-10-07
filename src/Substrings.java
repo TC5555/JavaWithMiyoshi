9e1772ecb013c8e0c43252458b00d4188cecb155
Tyler Carter <70717266+TC5555@users.noreply.github.com>
2021-10-07 10:37:24 -0700

    Finished Substrings

--- src/Substrings.java
+++ src/Substrings.java
@@ -1,25 +1,21 @@
 
 public class Substrings {
-    public static String[] SubS(String a, int b) {
-        String[] output = new String[(a.length() / b) + 1];
-        for(int i = 0; i < (a.length() / b) + 1; i++) {
+    public static void SubS(String a, int b) {
+        String[] output = new String[a.length() / b];
+        int i;
+        for(i = 0; i < a.length() / b; i++) {
             int bi = b * i;
             String ASub = a.substring(bi, b + bi);
-            if (ASub.length() < b + bi) {
-                String TooMany = ASub;
-                output[i] = TooMany;
-                break;
-            }
             output[i] = ASub; }
-        return output;
+        int bi = b * i;
+        for(int j = 0; j < a.length() / b; j++) {
+            System.out.println(output[j]); }
+        System.out.println(a.substring(bi));
     }
 
     public static void main(String[] args) {
-        String a = "qwertyuiopasdfg";
-        int b = 7;
-        String[] c = SubS(a, b);
-        for(int i = 0; i < a.length() / b; i++) {
-        System.out.println(c[i]); }
-        //System.out.println(a.substring(1,5));
+        String a = "qwert";
+        int b = 2;
+        SubS(a, b);
     }
 }
