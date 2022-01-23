950d16da0ffc9b5d11b4157ab0308e81ce634b1c
Tyler Carter <cartert@rsd407.org>
2022-01-23 15:32:22 -0800

    Made it so scale list uses a method rather than all in main

--- src/scaleList.java
+++ src/scaleList.java
@@ -1,21 +1,25 @@
 import java.util.*;
 
 public class scaleList {
+    public static ArrayList<Integer> ScaleByK(ArrayList<Integer> ToScale) {
+        ArrayList<Integer> Scaled = new ArrayList<>();
+        for(int i = 0; i < ToScale.size(); i++) {
+            int k=ToScale.get(i);
+            System.out.println(k);
+            for(int j = 0; j < k; j++) {
+                Scaled.add(k);
+            }
+        }
+        return Scaled;
+    }
     public static void main(String[] args) {
         ArrayList<Integer> ToScale = new ArrayList<>();
-        ArrayList<Integer> Scaled = new ArrayList<>();
-        int k;
+
         ToScale.add(4);
         ToScale.add(1);
         ToScale.add(2);
         ToScale.add(0);
         ToScale.add(3);
-        for(int i = 0; i < ToScale.size(); i++) {
-            k=ToScale.get(i);
-            System.out.println(k);
-            for(int j = 0; j < k; j++) {
-             Scaled.add(k);
-            }
-    }
-            System.out.print(Scaled);
+
+            System.out.print(ScaleByK(ToScale));
 }}
