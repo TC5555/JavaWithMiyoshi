b2c1fe0a0b0c111bb07bf67de06260114e5359f5
Tyler Carter <70717266+TC5555@users.noreply.github.com>
2022-03-10 09:56:43 -0800

    finished selectionsortdual

--- src/SelectionSortDual.java
+++ src/SelectionSortDual.java
@@ -5,25 +5,36 @@ public class SelectionSortDual {
         list[j] = temp;
     }
     public static int[] selectionSort(int[] a) {
-        for (int i = a.length - 1; i > 0; i--) { //this for loop happens 'a' times.
-            int largest = i, smallest = i;
-            for (int j = i; j > 0; j--) {
-                if (a[j] > a[largest]) {
-                    largest = j;
+        for (int i =a.length - 1; i > 0; i--) { //this for loop happens 'a' times.
+            int largest = i, smallest = (a.length-1) - i;
+            for (int j = (a.length-1) - i; j < a.length; j++) {
+                if (a[(a.length-1) -j] > a[largest]) {
+                    largest = (a.length-1) -j;
                 }
-            }
-            for (int j = i + 1; j < a.length; j++) {
                 if (a[j] < a[smallest]) {
                     smallest = j;
                 }
             }
+            if((a.length-1) -i == largest && i == smallest) {
+                swap(a, smallest, largest);
+            }
+            else if((a.length-1) -i == largest) {
+            swap(a, (a.length-1) -i, smallest);
+                swap(a, smallest, i);
+            }
+            else if(i == smallest){
             swap(a, i, largest);
-            swap(a, i, smallest);
+                swap(a, largest, i);}
+            else {
+                swap(a, i, largest);
+            swap(a, (a.length-1) -i, smallest);
+            }
+
         }
         return a;
     }
     public static void main(String[] args) {
-        int[] a = {1, 3, 4, 6, 107, 23, 4, 9, 54};
+        int[] a = {900,2,1,1,1,1,1,1,1,1,2,2,2,2,2,3,3,3,3,3451212,-200000,1213,-21,2121,1,1,2,2,2,4,4,4,4,4,4,4,2126,1};
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i]+ " ");
     }
