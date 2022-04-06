ee712d34f298f76eaf4fb1f4bed4d6bd762f15f0
Tyler Carter <70717266+TC5555@users.noreply.github.com>
2022-04-06 10:54:49 -0700

    Finished isReverse

--- src/IsReverse.java
+++ src/IsReverse.java
@@ -1,20 +1,21 @@
 public class IsReverse {
    public static boolean isRev(String word1, String word2){
 
-      if(word1.isEmpty() || (word1.length() <= 1 && word1.equals(word2))){
+      int length = word1.length();
+
+      if(length <= 1 && word1.equals(word2)){
          return true;
       }
 
-      if(word1.length() != word2.length()){
+      if(length != word2.length()){
          return false;
       }
 
-      int length = word1.length() -1;
-         return Character.toUpperCase(word1.charAt(0)) == Character.toUpperCase(word2.charAt(length)) && isRev(word1.substring(1, length +1), word2.substring(0, length));
+         return Character.toUpperCase(word1.charAt(0)) == Character.toUpperCase(word2.charAt(length)) && isRev(word1.substring(1, length), word2.substring(0, length-1));
       }
    public static void main(String[] args){
-      String word1 = "";
-      String word2 = "";
+      String word1 = "OLLEh";
+      String word2 = "Hello";
       System.out.print(isRev(word1, word2));
    }
 }
